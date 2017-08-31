686545d3d22f6467ae71a1811d3acf1592d20259
Vishesh 'IronEagle' Bangotra <vishesh@thevetted.com>
2017-08-31 14:59:05 +0530

    removed stray code

--- webhooks/urls.py
+++ webhooks/urls.py
@@ -5,10 +5,7 @@ import views
 from django.conf.urls import url
 
 
-# namespace
 urlpatterns = [
-    'lifecycles.views',
-
     # POST: WEBHOOKS #
     url(r'^email-activities/$', views.email_activities_webhook, name='email_activities'),
     url(r'^incoming-mail/$', views.incoming_mail_webhook, name='incoming_mail'),
